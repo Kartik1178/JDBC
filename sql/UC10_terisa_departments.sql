9fb2a182481ce32a134b53dbe2d14ec8136c9645
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-11 09:06:45 +0530

    [Kartikeya]: Inserted Terisa into Sales and Marketing departments

---
+++ sql/UC10_terisa_departments.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee_payroll
+(name,phone_number,address,department,gender,basic_pay,deductions,taxable_pay,income_tax,net_pay,start)
+VALUES
+('Terisa','9999999999','NY','Sales','F',3000000,100000,2900000,500000,2400000,'2018-01-03'),
+
+('Terisa','9999999999','NY','Marketing','F',3000000,100000,2900000,500000,2400000,'2018-01-03');
