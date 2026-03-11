fb5f68f77f3b6632852852854a3c42cb4663bfac
Kartikeya Pusapati <swagboss600@gmail.com>
2026-03-11 12:43:54 +0530

    [Kartikeya]: Implemented JDBC UC1 database connection

--- src/main/java/com/bridgelabz/payroll/PayrollDBService.java
+++ src/main/java/com/bridgelabz/payroll/PayrollDBService.java
@@ -2,6 +2,18 @@ package com.bridgelabz.payroll;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+ PayrollDBService
+
+ This class is responsible for establishing
+ a connection between the Java application
+ and the MySQL payroll_service database.
+
+ It loads the JDBC driver and returns
+ a connection object used by the service layer.
+*/
 
 public class PayrollDBService {
 
@@ -11,14 +23,17 @@ public class PayrollDBService {
     private static final String USER = "root";
     private static final String PASSWORD = "root";
 
-    public Connection getConnection() throws Exception {
+    // method to establish database connection
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
 
+        // load JDBC driver
         Class.forName("com.mysql.cj.jdbc.Driver");
 
+        // create connection
         Connection connection =
                 DriverManager.getConnection(URL, USER, PASSWORD);
 
-        System.out.println("Connection established");
+        System.out.println("Connection Established");
 
         return connection;
     }
